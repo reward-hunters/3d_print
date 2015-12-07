42879130445ab7645c34c61dc569aa99e30cb8f9
reward-hunters <hunters.reward@gmail.com>
2015-12-07 21:48:18 +0500

    баги

--- RH.HeadShop/Render/ctrlTemplateImage.cs
+++ RH.HeadShop/Render/ctrlTemplateImage.cs
@@ -1874,13 +1874,6 @@ namespace RH.HeadShop.Render
             }
             SetTemplateImage(ProgramCore.Project.ProfileImage, false);
 
-
-
-            ProfileScreenTopLocation = GetScreenPoint(profileControlPoints[0].Value);
-            ProgramCore.Project.ProfileEyeLocation = ProfileScreenEyeLocation = GetScreenPoint(profileControlPoints[1].Value);
-            ProgramCore.Project.ProfileMouthLocation = ProfileScreenMouthLocation = GetScreenPoint(profileControlPoints[2].Value);
-            ProfileScreenBottomLocation = GetScreenPoint(profileControlPoints[3].Value);
-
             RecalcProfilePoints();
         }
 
@@ -1897,6 +1890,14 @@ namespace RH.HeadShop.Render
 
         public void RecalcProfilePoints()
         {
+
+            if (profileControlPoints.Count > 0 &&(ProgramCore.Project.ProfileEyeLocation == Vector2.Zero || ProgramCore.Project.ProfileMouthLocation == Vector2.Zero))
+            {
+                ProfileScreenTopLocation = GetScreenPoint(profileControlPoints[0].Value);
+                ProgramCore.Project.ProfileEyeLocation = ProfileScreenEyeLocation = GetScreenPoint(profileControlPoints[1].Value);
+                ProgramCore.Project.ProfileMouthLocation = ProfileScreenMouthLocation = GetScreenPoint(profileControlPoints[2].Value);
+                ProfileScreenBottomLocation = GetScreenPoint(profileControlPoints[3].Value);
+            }
 
             #region Пересчитываем точки справа на лево
 
